d3ec4ca2b8862ba77b1a66ef8096340b3f94fd20
William Smith <williamsmithe@icloud.com>
2023-10-05 09:30:27 -0700

    Update File.Open invocation in the Workbook constructor to specify FileShare.Read enum.

---
+++ Exceleration/CellListExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Exceleration
+{
+    public static class CellListExtensions
+    {
+        public static Cell GetFirstCellByRowNumber(this List<Cell> cells, int rowNumber)
+        {
+            return cells.First(x => x.Row == rowNumber);
+        }
+
+        public static Cell GetFirstCellByColumnNumber(this List<Cell> cells, int columnNumber)
+        {
+            return cells.First(x => x.Column == columnNumber);
+        }
+
+        public static Cell GetFirstCellByColumnLetter(this List<Cell> cells, string columnLetter)
+        {
+            return cells.First(x => x.ColumnLetter.Equals(columnLetter));
+        }
+    }
+}
--- Exceleration/Workbook.cs
+++ Exceleration/Workbook.cs
@@ -19,7 +19,7 @@ namespace Exceleration
 
             Sheets = new List<Worksheet>();
 
-            using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
             using var reader = ExcelReaderFactory.CreateReader(stream);
 
             var result = reader.AsDataSet(new ExcelDataSetConfiguration());
