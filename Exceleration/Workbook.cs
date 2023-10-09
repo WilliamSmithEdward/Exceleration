0d1664e5e5473a6dc9d98b459900bb7b0728849f
William Smith <williamsmithe@icloud.com>
2023-10-09 10:14:42 -0700

    Fix bug in Workbook constructor

--- Exceleration/Workbook.cs
+++ Exceleration/Workbook.cs
@@ -47,7 +47,7 @@ namespace Exceleration
 
             Sheets = new List<Worksheet>();
 
-            using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            using var stream = File.Open(FilePath, FileMode.Open, FileAccess.Read);
             using var reader = ExcelReaderFactory.CreateReader(stream);
 
             var result = reader.AsDataSet(new ExcelDataSetConfiguration());
